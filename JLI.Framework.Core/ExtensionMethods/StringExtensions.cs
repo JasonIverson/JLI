6619de92fc3c584258670dd94eec30acf9b3b903
Jason Iverson <hello@jason-iverson.com>
2022-05-07 12:00:53 -0700

    A few more minor additions

--- JLI.Framework.Core/ExtensionMethods/StringExtensions.cs
+++ JLI.Framework.Core/ExtensionMethods/StringExtensions.cs
@@ -9,6 +9,24 @@ namespace JLI.Framework.Core
     public static class StringExtensions
     {
 
+        #region String Extensions
+
+        /// <summary>
+        /// Returns a "string in this format" as a "String In This Format"
+        /// </summary>
+        /// <param name="source"></param>
+        /// <returns></returns>
+        public static String ToTitleCase(this String source)
+        {
+            if (String.IsNullOrWhiteSpace(source))
+                return source;
+            return System.Globalization.CultureInfo.CurrentCulture.TextInfo.ToTitleCase(source);
+        }
+
+        #endregion
+
+        #region IEnumerable<String> Extensions
+
         /// <summary>
         /// Returns the first non-null or white space value, or null in the event that no values are found.
         /// </summary>
@@ -30,6 +48,8 @@ namespace JLI.Framework.Core
             }
             return returnValue;
         }
+
+        #endregion IEnumerable<String> Extensions
 
     }
 }
---
+++ JLI.Framework.Models/ModelBase.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JLI.Framework.Models
+{
+    public abstract class ModelBase
+    {
+
+        public Guid Id { get; set; }
+
+        public DateTime CreatedDateUtc { get; set; }
+
+        public DateTime? UpdatedDateUtc { get; set; }
+
+    }
+}
