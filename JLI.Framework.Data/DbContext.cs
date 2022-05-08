116b87b13784c469773d2e391cef98e198f9a1bc
Jason Iverson <hello@jason-iverson.com>
2022-05-08 13:11:47 -0700

    Editor config; added custom DbContext and EF Core references

---
+++ JLI.Framework.Data/DbContext.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JLI.Framework.Data
+{
+    public class DbContext
+        : Microsoft.EntityFrameworkCore.DbContext
+    {
+
+    }
+}
