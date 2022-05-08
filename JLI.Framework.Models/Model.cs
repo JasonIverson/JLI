9fa860c3bdb51a4059a7c83be062f928dac43323
Jason Iverson <hello@jason-iverson.com>
2022-05-08 10:44:59 -0700

    Added data and console projects; renamed models project

--- JLI.Framework.Models/Model.cs
+++ JLI.Framework.Models/Model.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace JLI.Framework.Models
+namespace JLI.Framework.Data.Models
 {
     public abstract class Model : ModelBase<Guid>
     {
