3708c4cc82ff9f9feaa4dd91a6bdbb91555ac3df
Jason Iverson <hello@jason-iverson.com>
2022-05-08 17:13:21 -0700

    Added settings to console app.

--- Apps/ConsoleApp/Program.cs
+++ Apps/ConsoleApp/Program.cs
@@ -1,9 +1,28 @@
 ﻿using System;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Configuration;
 
 namespace ConsoleApp {
     class Program {
         static void Main(string[] args) {
-            Console.WriteLine("Hello World!");
+
+            IConfiguration config = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.json", false, true)
+                .AddEnvironmentVariables()
+                .Build();
+
+            Settings settings = config.GetRequiredSection("Settings").Get<Settings>();
+
+            Console.WriteLine($"{new String('-', 40)}");
+            Console.WriteLine($"{settings.Value}!");
+            Console.WriteLine($"{new String('-', 40)}");
+
+            Console.WriteLine();
+            Console.WriteLine("Press {Enter} to quit.");
+            Console.ReadLine();
         }
     }
+
+    
+
 }
