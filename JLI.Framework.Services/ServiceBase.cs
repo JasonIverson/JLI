24ed6ee41604c833b8b1b4feb4dafa63d0b851c4
Jason Iverson <hello@jason-iverson.com>
2022-08-25 17:44:59 -0700

    Started on services project

---
+++ JLI.Framework.Services/ServiceBase.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace JLI.Framework.Services
+{
+    public abstract class ServiceBase
+    {
+
+        #region Properties
+
+        protected IServiceProvider ServiceProvider { get; private set; }
+
+        #endregion Properties
+
+        #region Constructor(s)
+
+        protected ServiceBase(IServiceProvider serviceProvider) {
+            this.ServiceProvider = serviceProvider;
+        }
+
+        #endregion Constructor(s)
+
+    }
+}
