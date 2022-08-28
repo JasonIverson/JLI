39e4fc4922fadc210ac9f1346584df8349afca71
Jason Iverson <hello@jason-iverson.com>
2022-08-27 17:50:09 -0700

    Added extensions to localize UTC datetime values

---
+++ JLI.Framework.Core/ExtensionMethods/DateTimeExtensions.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace System {
+    public static class DateTimeExtensions {
+
+        #region DateTime
+
+        /// <summary>
+        /// Localizes a <see cref="DateTime"/> in Utc to the Timezone specified by <paramref name="toTimezoneId"/>.
+        /// </summary>
+        /// <param name="dateTimeUtc"></param>
+        /// <param name="toTimezoneId"></param>
+        /// <returns></returns>
+        public static DateTime Localize(this DateTime dateTimeUtc, String toTimezoneId) {
+            TimeZoneInfo timezoneInfo = TimeZoneInfo.FindSystemTimeZoneById(toTimezoneId);
+            DateTime returnValue = TimeZoneInfo.ConvertTimeFromUtc(dateTimeUtc.ToUniversalTime(), timezoneInfo);
+            return returnValue;
+        }
+
+        #endregion DateTime
+
+        #region DateTime?
+
+        /// <summary>
+        /// Localizes a <see cref="DateTime?"/> in Utc to the Timezone specified by <paramref name="toTimezoneId"/>.
+        /// </summary>
+        /// <param name="dateTimeUtc"></param>
+        /// <param name="toTimezoneId"></param>
+        /// <returns></returns>
+        public static DateTime? Localize(this DateTime? dateTimeUtc, String toTimezoneId) {
+            if (!dateTimeUtc.HasValue)
+                return dateTimeUtc;
+
+            DateTime returnValue = dateTimeUtc.Value.Localize(toTimezoneId);
+            return returnValue;
+        }
+
+        #endregion DateTime?
+
+    }
+}
