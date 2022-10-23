936aaf4970e4ef142e80ae1d6d119e9978e59c96
Jason Iverson <hello@jason-iverson.com>
2022-10-22 19:18:33 -0700

    Clean up

---
+++ JLI.Framework.Data/Attributes/DataAnnotations/FormalNameLengthAttribute.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JLI.Framework.Data {
+    public class FormalNameLengthAttribute : System.ComponentModel.DataAnnotations.StringLengthAttribute {
+
+        #region Constructor(s)
+
+        public FormalNameLengthAttribute()
+            : base(64) { }
+
+        #endregion Constructor(s)
+
+    }
+}
