8d9f33d6591c578e6a2c48a6345fea40c3da0f2b
Jason Iverson <hello@jason-iverson.com>
2022-12-18 14:05:04 -0800

    Create QuerySettings(TModel).cs

---
+++ JLI.Framework.Data/QuerySettings(TModel).cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JLI.Framework.Data {
+    public class QuerySettings<TModel>
+        where TModel : Models.Model, new() {
+
+        #region Constructor(s)
+
+        public QuerySettings(bool trackingEnabled) {
+            this.TrackingEnabled = trackingEnabled;
+        }
+
+        #endregion Constructor(s)
+
+        #region Properties
+
+        public bool TrackingEnabled { get; set; }
+
+        #endregion Properties
+
+    }
+}
