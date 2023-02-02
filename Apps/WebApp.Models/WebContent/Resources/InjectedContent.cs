722a4affe4b83173cb00910a2cfc8add8da328c1
Jason Iverson <hello@jason-iverson.com>
2023-02-01 20:17:21 -0800

    Update InjectedContent.cs

--- Apps/WebApp.Models/WebContent/Resources/InjectedContent.cs
+++ Apps/WebApp.Models/WebContent/Resources/InjectedContent.cs
@@ -6,6 +6,8 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace WebApp.Models.WebContent.Resources {
+
+    // TODO:  Update object model so InjectedContent can reference both PageTemplate and Page
     public class InjectedContent : ResourceBase {
 
         public ContentTypes Type { get; set; }
