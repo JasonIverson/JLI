7ca56feb556fb7e9196b3c033f7b98f277fd59bb
Jason Iverson <hello@jason-iverson.com>
2023-03-25 12:20:16 -0700

    Implementing some object model in _Layout

--- Apps/WebApp/Controllers/HomeController.cs
+++ Apps/WebApp/Controllers/HomeController.cs
@@ -6,35 +6,89 @@ using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 using WebApp.Models;
+using WebApp.Models.Common;
+using WebApp.Models.Profiles;
+using WebApp.Models.WebContent;
 
-namespace WebApp.Controllers
-{
-    public class HomeController : Controller
-    {
+namespace WebApp.Controllers {
+    public class HomeController : Controller {
         private readonly ILogger<HomeController> _logger;
 
-        public HomeController(ILogger<HomeController> logger)
-        {
+        public HomeController(ILogger<HomeController> logger) {
             _logger = logger;
         }
 
-        public IActionResult Index()
-        {
-            return View();
+        
+
+        private (PageTemplate, Page) GetObject() {
+            Profile profile = new() {
+                Id = Guid.NewGuid(),
+                CreatedDateUtc = DateTime.UtcNow,
+                Name = "Jason Iverson Consulting",
+                PrimaryContact = new() {
+                    Id = Guid.NewGuid(),
+                    Email = "hello@jason-iverson.com",
+                    Address = new() {
+                        Id = Guid.NewGuid(),
+                        Line1 = "PO Box 277298",
+                        City = "Sacramento",
+                        State = States.CALIFORNIA.Abbreviation,
+                        PostalCode = "95827"
+                    },
+                    FirstName = "Jason",
+                    LastName = "Iverson",
+                    PhoneNumber = "9166680068",
+                    Title = "Proprietor",
+                    CreatedDateUtc = DateTime.UtcNow
+                },
+                SocialMediaAccounts = new() {
+                    new SocialMediaAccount() {
+                        Platform = SocialMediaPlatforms.Twitter,
+                        Order = 0,
+                        UserName = "JasonAtSBWP"
+                    },
+                    new SocialMediaAccount() {
+                        Platform = SocialMediaPlatforms.LinkedIn,
+                        Order = 1,
+                        UserName = "jasoniverson"
+                    }
+                }
+            };
+
+            PageTemplate pageTemplate = new() {
+                Id = Guid.NewGuid(),
+                Profile = profile
+            };
+            Page page = new() {
+                Id = Guid.NewGuid(),
+                ProfileId = profile.Id,
+                Profile = profile,
+                Name = "Home",
+                Type = PageTypes.Homepage,
+                Metadata = new() {
+                    Title = "Jason Iverson Consulting",
+                    Description = "Sacramento based Microsoft technologies consultant with over 15 years of experience delivering business solutions on time and under budget",
+                    Keywords = "Sacramento, Full Stack Developer, Back End Developer, Software Consultant, Web Develper, Software Engineer"
+                }
+            };
+
+            return (pageTemplate, page);
+        }
+
+        public IActionResult Index() {
+            return this.View(this.GetObject());
         }
 
         public IActionResult Elements() {
-            return this.View();
+            return this.View(this.GetObject());
         }
 
-        public IActionResult Privacy()
-        {
+        public IActionResult Privacy() {
             return View();
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
+        public IActionResult Error() {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
     }
