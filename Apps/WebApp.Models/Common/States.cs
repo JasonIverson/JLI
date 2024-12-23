641b7e4e258a7efa2588742f60af326075992aa6
Jason Iverson <Jason.L.Iverson@gmail.com>
2024-12-23 10:48:35 -0800

    Clean up

--- Apps/WebApp.Models/Common/States.cs
+++ Apps/WebApp.Models/Common/States.cs
@@ -1,13 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Reflection.PortableExecutable;
-using System.Text;
-using System.Threading.Tasks;
-using static System.Net.Mime.MediaTypeNames;
-using System.Xml.Linq;
-using System.Reflection;
-using System.Collections.ObjectModel;
+﻿using System.Collections.ObjectModel;
 
 namespace WebApp.Models.Common {
     public static class States {
--- Apps/WebApp.Models/Profiles/Enums.cs
+++ Apps/WebApp.Models/Profiles/Enums.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace WebApp.Models.Profiles {
+﻿namespace WebApp.Models.Profiles {
 
     public enum SocialMediaPlatforms : byte {
 
